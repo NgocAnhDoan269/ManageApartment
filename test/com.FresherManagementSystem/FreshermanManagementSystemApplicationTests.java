106d3200642db9da8cdd6370c49558a0cd72ad97
NgocAnhDoan269 <doanngocanh_t66@hus.edu.vn>
2024-09-13 08:27:25 +0700

    Create FreshermanManagementSystemApplicationTests.java

---
+++ test/com.FresherManagementSystem/FreshermanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreshermanManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshermanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
