7d238d599d15193e0f6fe8083d2c136ecdaee4ea
NgocAnhDoan269 <doanngocanh_t66@hus.edu.vn>
2024-09-13 08:25:37 +0700

    Add files via upload

---
+++ com.FresherManagementSystem/FreshermanManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.FreshermanManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshermanManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshermanManagementSystemApplication.class, args);
+	}
+
+}
